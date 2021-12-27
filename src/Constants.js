37a7e8755ba07c6830004511a2462eda2b145c0a
Kaitlin <kaitlin.decf@gmail.com>
2021-12-27 00:07:55 -0700

    Projects

--- src/Constants.js
+++ src/Constants.js
@@ -5,3 +5,29 @@ export const description_3 = ". I've had the opportunity to work in a mulititude
 export const linkedin = "https://www.linkedin.com/in/kaitlin-decf/"
 export const github = "https://github.com/kaitlin31415"
 export const ucalgary = "https://science.ucalgary.ca/computer-science"
+
+export const proj_1_name = "Canadian Political Diversification Through Network Analysis"
+export const proj_1_description = "With NetworkX and Gephi, Canadian Members of Parliament and Activist Organizations Twitter accounts retweets were analyzed for the brief period leading up to the 2021 federal elections. The object was to identify echo chambers that plague the scene and to find remedies in the form of bridge nodes that would serve to deliver a broader view for the people following. We found six main communities in Canadian politics, which we applied centrality measures, and information diffusion techniques to learn more about Canadian political interactions."
+
+export const proj_2_name = "Canadian Energy Regulator Conditions Visualization"
+export const proj_2_description = "Worked on frontend data visualization with React JS. The Canada Energy Regulator (CER) wanted their data to be more transparent and accessible to all external audiences: regulators, investors, pipeline operators and the general public. The CER was looking for a way for everyone to not just see the data but also interact with it. The main goal was to be able to gain insights and provide a storytelling platform for all peoples, leaving hundred line spreadsheets behind and translating them into meaningful information."
+export const proj_2_url_1 = "https://apps2.cer-rec.gc.ca/conditions/"
+
+export const proj_3_name = "J-- Compiler"
+export const proj_3_description = "Created A J-- Compiler for CPSC 411 in C++ with tools Yacc and Bison. This language consisted of parsing simple programming statments and consisted of a Scanner, Parser, Semantic Analyzer, and Code generator that generates J-- code into Web Assembly Text. Code is available for discussion upon request."
+
+
+export const proj_4_name = "And Tree Based Search"
+export const proj_4_description = "Created a course scheduler optimization tool in Java that used the and-tree-based search to explore different possiblities and optimize. Code is available for discussion upon request."
+
+export const proj_5_name = "Hidden In Plain Sight "
+export const proj_5_description = "Applied steganography to hide images in pictures based on a secret hidden in one pixel. The image with the hidden message can be fed back into the program and the message recieved."
+export const proj_5_url_1="https://github.com/kaitlin31415/HiddenInPlainSight"
+
+
+
+
+
+
+
+
